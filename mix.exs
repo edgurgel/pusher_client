5c138050bafab546b584b7d9b83d0aec8b43aa41
Eduardo Gurgel <edgurgel@gmail.com>
2013-12-24 01:11:24 -0300

    Support Elixir ~> 0.11.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PusherClient.Mixfile do
   def project do
     [ app: :pusher_client,
       version: "0.0.1",
-      elixir: "~> 0.10.3",
+      elixir: "~> 0.11.2 or ~> 0.10.3",
       deps: deps(Mix.env) ]
   end
 
