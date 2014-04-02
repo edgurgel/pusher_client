73760f560ed926c2d2d8bd12fffb330ba08bd1ba
Eduardo Gurgel <eduardo@gurgel.me>
2014-04-02 22:31:27 +1300

    Remove warnings
    [ci skip]

--- lib/pusher_client/ws_handler.ex
+++ lib/pusher_client/ws_handler.ex
@@ -42,15 +42,15 @@ defmodule PusherClient.WSHandler do
   end
 
   @doc false
-  def websocket_terminate({close, 4001, _message}, _conn_state, _state) do
+  def websocket_terminate({_close, 4001, _message}, _conn_state, _state) do
     Lager.error "Wrong app_key"
     :ok
   end
-  def websocket_terminate({close, 4007, _message}, _conn_state, _state) do
+  def websocket_terminate({_close, 4007, _message}, _conn_state, _state) do
     Lager.error "Pusher server does not support current protocol #{@protocol}"
     :ok
   end
-  def websocket_terminate({close, code, payload }, _conn_state, _state) do
+  def websocket_terminate({_close, code, payload }, _conn_state, _state) do
     Lager.info "Websocket close with code #{code} and payload '#{payload}'."
     :ok
   end
